6a03e526d4f6e8d9a4e89aa4569a92b7efbd2cca
Luke Scott <luke@webconnex.com>
2015-07-27 11:23:52 -0600

    Change plugin to use ES5 syntax to avoid compilation

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-export default function({Plugin, types: t}) {
-
-    let addClassName = function(node, partner) {
-        if(node.id == null) { // If class name not defined
-            return node
-        }
-
-        node.body.body.unshift({
-            type: "ClassProperty",
-            key: {
-                name: "className",
-                type: "Identifier"
-            },
-            value: {
-                value: node.id.name,
-                type: "Literal"
-            },
-            static: true,
-            computed: false
-        })
-
-        return node
-    }
-
-    return new Plugin("class-name", {
-        visitor: {
-            ClassDeclaration: addClassName,
-            ClassExpression: addClassName
-        }
-    })
-}
+module.exports = function(babel) {
+	var addClassName = function(node) {
+		if(node.id != null) {
+			node.body.body.unshift({
+				type: "ClassProperty",
+				key: {
+					name: "className",
+					type: "Identifier"
+				},
+				value: {
+					value: node.id.name,
+					type: "Literal"
+				},
+				static: true,
+				computed: false
+			});
+		}
+		return node;
+	};
+	return new babel.Plugin("class-name", {
+		visitor: {
+			ClassDeclaration: addClassName,
+			ClassExpression: addClassName
+		}
+	});
+};
